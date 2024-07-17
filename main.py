f0a86fc9f61072824755d609895f928fb50afa83
WuTianchen <wutianchen@zerozero.cn>
2024-07-17 17:57:39 +0800

    import new method

--- main.py
+++ main.py
@@ -272,9 +272,30 @@ def match_template_multi_scale_angle(image, template, scales, angles, best_match
     return best_match
 
 
+def preprocess(img):
+    img_blur = cv2.GaussianBlur(img, (5, 5), 1)
+    img_canny = cv2.Canny(img_blur, 50, 50)
+    kernel = np.ones((3, 3))
+    img_dilate = cv2.dilate(img_canny, kernel, iterations=2)
+    img_erode = cv2.erode(img_dilate, kernel, iterations=1)
+    return img_erode
+
+def find_tip(points, convex_hull):
+    length = len(points)
+    indices = np.setdiff1d(range(length), convex_hull)
+
+    for i in range(2):
+        j = indices[i] + 2
+        if j > length - 1:
+            j = length - j
+        if np.all(points[j] == points[indices[i - 1] - 2]):
+            return tuple(points[j])
+
+
+
 if __name__ == '__main__':
     image_folder = './image'
-    destination_text = "儿科"
+    destination_text = "输液区"
     debug = True
 
     ocr = PaddleOCR(use_angle_cls=True, lang="ch", show_log=False, use_gpu=True)
@@ -313,75 +334,114 @@ if __name__ == '__main__':
                     cv2.rectangle(image_show, text_target.coordinates.cv_top_left(), text_target.coordinates.cv_bottom_right(), (0, 255, 0), 2)
                     cv2.imshow("Result", image_show)
                     cv2.waitKey(0)
-                # detect_arrow(mask_image, text_target, debug)
-                # 模板匹配
-                template = cv2.imread("./template/image.png", cv2.IMREAD_GRAYSCALE)
-                # 缩小2x
-                template = cv2.resize(template, (0, 0), fx=0.5, fy=0.5)
-                # 模板预处理，二值化并留下黑色部分
-                _, template = cv2.threshold(template, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-                # template = cv2.bitwise_not(template)
-                # template 逆时针旋转90度 构建新的模板
-                template_list = [template]
-                template = cv2.rotate(template, cv2.ROTATE_90_COUNTERCLOCKWISE)
-                template_list.append(template)
-                template = cv2.rotate(template, cv2.ROTATE_90_COUNTERCLOCKWISE)
-                template_list.append(template)
+
+
+                # top_left, bottom_right = text_target.coordinates.bounding_box()
+                # extend_pixel = (bottom_right[0] - top_left[0]) * 2
+                # top_left = (max(0, top_left[0] - extend_pixel), max(0, top_left[1] - extend_pixel))
+                # bottom_right = (min(image.shape[1], bottom_right[0] + extend_pixel), min(image.shape[0], bottom_right[1] + extend_pixel))
+                # roi = mask_image[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+                # # resize 2x larger
+                # roi = cv2.resize(roi, (0, 0), fx=2, fy=2)
+                
+                # _, roi = cv2.threshold(roi, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                # contours, hierarchy = cv2.findContours(preprocess(roi), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+                # imgShow = cv2.cvtColor(roi, cv2.COLOR_GRAY2BGR)
+                # # draw all contours
+                # cv2.drawContours(imgShow, contours, -1, (255, 0, 0), 1)
+                # if debug:
+                #     cv2.imshow("Contours", imgShow)
+                #     cv2.waitKey(0)
+
+                # for cnt in contours:
+                #     peri = cv2.arcLength(cnt, True)
+                #     approx = cv2.approxPolyDP(cnt, 0.025 * peri, True)
+                #     hull = cv2.convexHull(approx, returnPoints=False)
+                #     sides = len(hull)
+                #     # draw current cnt
+
+                #     if 6 > sides > 3 and sides + 2 == len(approx):
+                #         print("Found arrow")
+                #         arrow_tip = find_tip(approx[:,0,:], hull.squeeze())
+                #         cv2.drawContours(imgShow, [cnt], -1, (0, 0, 255), 1)
+                #         # show info
+                #         if arrow_tip:
+                #             cv2.drawContours(imgShow, [cnt], -1, (0, 255, 0), 3)
+                #             cv2.circle(imgShow, arrow_tip, 3, (0, 255, 0), cv2.FILLED)
+                # cv2.imshow("Arrow", imgShow)
+                # cv2.waitKey(0)
+
+                if True:
+                    # detect_arrow(mask_image, text_target, debug)
+                    # 模板匹配
+                    template = cv2.imread("./template/image.png", cv2.IMREAD_GRAYSCALE)
+                    # 缩小2x
+                    template = cv2.resize(template, (0, 0), fx=0.5, fy=0.5)
+                    # 模板预处理，二值化并留下黑色部分
+                    _, template = cv2.threshold(template, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                    template = cv2.bitwise_not(template)
+                    # template 逆时针旋转90度 构建新的模板
+                    template_list = [template]
+                    template = cv2.rotate(template, cv2.ROTATE_90_COUNTERCLOCKWISE)
+                    template_list.append(template)
+                    template = cv2.rotate(template, cv2.ROTATE_90_COUNTERCLOCKWISE)
+                    template_list.append(template)
 
 
 
-                # scales = [0.5, 0.75, 1.0]
-                scales = [0.75, 1.0, 1.5]
-                angles = [-15, 0, 15]
-                top_left, bottom_right = text_target.coordinates.bounding_box()
-                extend_pixel = (bottom_right[0] - top_left[0]) * 2
-                top_left = (max(0, top_left[0] - extend_pixel), max(0, top_left[1] - extend_pixel))
-                bottom_right = (min(image.shape[1], bottom_right[0] + extend_pixel), min(image.shape[0], bottom_right[1] + extend_pixel))
-                roi = mask_image[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
-                # OTSU
-                # roi = cv2.GaussianBlur(roi, (5, 5), 0)
-                _, roi = cv2.threshold(roi, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-                if debug:
-                    cv2.imshow("ROI", roi)
+                    # scales = [0.5, 0.75, 1.0]
+                    scales = [0.75, 1.0, 1.5]
+                    angles = [-15, 0, 15]
+                    top_left, bottom_right = text_target.coordinates.bounding_box()
+                    extend_pixel = (bottom_right[0] - top_left[0]) * 2
+                    top_left = (max(0, top_left[0] - extend_pixel), max(0, top_left[1] - extend_pixel))
+                    bottom_right = (min(image.shape[1], bottom_right[0] + extend_pixel), min(image.shape[0], bottom_right[1] + extend_pixel))
+                    roi = mask_image[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+                    # OTSU
+                    # roi = cv2.GaussianBlur(roi, (5, 5), 0)
+                    _, roi = cv2.threshold(roi, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                    if debug:
+                        cv2.imshow("ROI", roi)
+                        cv2.waitKey(0)
+
+                    best_match = {
+                        "max_val": 0.0,
+                        "max_loc": None,
+                        "scale": None,
+                        "angle": None,
+                        "template":None,
+                        }
+
+                    for template in template_list:
+                        best_match = match_template_multi_scale_angle(roi, template, scales, angles, best_match)
+
+                    if best_match["max_val"] < 0.4 :
+                        print("Cannot find the target image")
+                        continue
+                    print(f"Best match at location {best_match['max_loc']} with scale {best_match['scale']} and angle {best_match['angle']}")
+                    print(f"Match value: {best_match['max_val']}")
+
+                    # if best_match['max_val'] > 0.3:
+                    # show best_match['template']
+                    cv2.imshow("Best Match", best_match['template'])
                     cv2.waitKey(0)
+                    # Draw rectangle around the best match
+                    h, w = best_match['template'].shape[:2]
+                    best_scale = best_match["scale"]
+                    best_angle = best_match["angle"]
 
-                best_match = {
-                    "max_val": 0.0,
-                    "max_loc": None,
-                    "scale": None,
-                    "angle": None,
-                    "template":None,
-                    }
+                    # Resize and rotate template to best match scale and angle
+                    scaled_template = cv2.resize(best_match['template'], (0, 0), fx=best_scale, fy=best_scale)
+                    h_scaled, w_scaled = scaled_template.shape[:2]
+                    # center = (w_scaled // 2, h_scaled // 2)
+                    # rotation_matrix = cv2.getRotationMatrix2D(center, best_angle, 1.0)
+                    # rotated_template = cv2.warpAffine(scaled_template, rotation_matrix, (w_scaled, h_scaled))
 
-                for template in template_list:
-                    best_match = match_template_multi_scale_angle(roi, template, scales, angles, best_match)
-
-                if best_match["max_val"] < 0.4 :
-                    print("Cannot find the target image")
-                    continue
-                print(f"Best match at location {best_match['max_loc']} with scale {best_match['scale']} and angle {best_match['angle']}")
-                print(f"Match value: {best_match['max_val']}")
-
-                # if best_match['max_val'] > 0.3:
-                # show best_match['template']
-                cv2.imshow("Best Match", best_match['template'])
-                cv2.waitKey(0)
-                # Draw rectangle around the best match
-                h, w = best_match['template'].shape[:2]
-                best_scale = best_match["scale"]
-                best_angle = best_match["angle"]
-
-                # Resize and rotate template to best match scale and angle
-                scaled_template = cv2.resize(best_match['template'], (0, 0), fx=best_scale, fy=best_scale)
-                h_scaled, w_scaled = scaled_template.shape[:2]
-                # center = (w_scaled // 2, h_scaled // 2)
-                # rotation_matrix = cv2.getRotationMatrix2D(center, best_angle, 1.0)
-                # rotated_template = cv2.warpAffine(scaled_template, rotation_matrix, (w_scaled, h_scaled))
-
-                top_left = best_match["max_loc"]
-                bottom_right = (top_left[0] + w_scaled, top_left[1] + h_scaled)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
+                    top_left = best_match["max_loc"]
+                    bottom_right = (top_left[0] + w_scaled, top_left[1] + h_scaled)
+                    cv2.waitKey(0)
+                    cv2.destroyAllWindows()
 
             else:
                 print(f"Cannot find the target text '{destination_text}'")
